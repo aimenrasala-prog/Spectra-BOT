fbef985fc8b31eba324258dc3d9b33e47bd70abe
aimenrasala-prog <aimenrasala@gmail.com>
2026-02-22 18:07:04 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+// Cargar Eventos automáticamente
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(path.join(eventsPath, file));
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.login(process.env.DISCORD_TOKEN);
+console.log("✅ Bot encendido y configurado");
